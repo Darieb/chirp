d47a62594f8da0b9b4816f75b258b8f8ad43d616
Declan Rieb <WD5EQY@arrl.net>
2025-10-05 17:07:20 -0600

    Not connected with radio data, but works corrrectly

--- chirp/drivers/template.py
+++ chirp/drivers/template.py
@@ -1,4 +1,5 @@
 # Copyright 2012 Dan Smith <dsmith@danplanet.com>
+# Copyright 2025 Declan Rieb <wd5eqy@arrl.net>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,6 +14,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+""" This copy of the CHIRP template driver 'template' is designed to test special
+    memories that can be accessed via bank mappings.
+    I've been trying to do that with ft1d.py without great success, so this
+    is a toy version.
+
+    It requires that CHIRP have the Dan's 'banks_with_specials' commit.
+"""
 from chirp import chirp_common, memmap
 from chirp import bitwise, directory
 
@@ -33,7 +41,7 @@ struct {
 struct {
     char name[16];
     u16  channels[10];
-} bank[2]; 
+} bank[5]; 
 """
 
 YAESU_PRESETS = {
@@ -80,44 +88,34 @@ def do_upload(radio):
 
 class TemplateBankModel(chirp_common.BankModel,
                         chirp_common.SpecialBankModelInterface):
-
-    def __init__(self, radio: object, name='Banks') -> None:
-        super().__init__(radio, name)
-        _banks = self._radio._memobj.bank
-        self._bank_mappings = []
-        for index, _bank in enumerate(_banks):
-            bank = chirp_common.Bank(self, f"{index}", f"BANK-{index}")
-            bank.index = index
-            self._bank_mappings.append(bank)
-
-    def get_num_mappings(self) -> int:
-        return len(self.bank)
-
-    def get_mappings(self):
-        return self._bank_mappings
-
-    def add_memory_to_mapping(self, memory: chirp_common.Memory,\
-            bank: chirp_common.Bank) -> None:
-        """ Include CHIRP Memory into a bank """
-        print(f'AM2M: {memory.number}, {type(bank)}={bank}')
-        _i = bank.index
-        _members = list(self._radio._memobj.bank[_i].channels)
-        _members.append(memory.number)
-        print(f'AM2M: {memory.number}, {_members}')
-
-    def remove_memory_from_mapping(self, bank: chirp_common.Bank) -> None:
-        print(f'RMfM: {bank}')
-
-    def get_mapping_memories(self, bank: chirp_common.Bank) -> list:
-        memories = []
-        return memories
-
-    def get_memory_mappings(self, memory: chirp_common.Memory) -> list:
-        banks = []
-        return banks
+    ''' Copied from fake.py driver, in the FakeBankModel object.
+        This implementation requires NO interaction with radio data.
+        '''
 
     def get_bankable_specials(self) -> list:
         return list(YAESU_PRESETS.keys())
+
+    def __init__(self, radio, name='Banks'):
+        super().__init__(radio, name)
+        self._banks = {i: set() for i in range(5)}
+
+    def get_num_mappings(self):
+        return len(self._banks)
+
+    def get_mappings(self):
+        return [chirp_common.Bank(self, i, 'Bank %i' % i) for i in range(5)]
+
+    def get_memory_mappings(self, memory):
+        banks = self.get_mappings()
+        in_banks = [i for i in range(5) if memory.number in self._banks[i]]
+        return [bank for bank in banks if bank.get_index() in in_banks]
+
+    def add_memory_to_mapping(self, memory, mapping):
+        self._banks[mapping.get_index()].add(memory.number)
+
+    def remove_memory_from_mapping(self, memory, mapping):
+        self._banks[mapping.get_index()].remove(memory.number)
+
 
 # Uncomment this to actually register this radio in CHIRP
 @directory.register
